653bbd6dc80ea44279ee5fed9ed87f636e9a9450
Madhur-Parashar <madhur.cs18@gmail.com>
2020-07-05 23:58:31 +0530

    Updates

---
+++ precache-manifest.3811abd8ddf88622e7199319b0e0d956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e13dcbbfa37e59fa723f9349ef30ec",
+    "url": "/react-bill-managment/index.html"
+  },
+  {
+    "revision": "9485de8c3835d4b8c257",
+    "url": "/react-bill-managment/static/css/main.df147f4a.chunk.css"
+  },
+  {
+    "revision": "219c3a74bba73acbc03c",
+    "url": "/react-bill-managment/static/js/2.bcdd39f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-bill-managment/static/js/2.bcdd39f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9485de8c3835d4b8c257",
+    "url": "/react-bill-managment/static/js/main.fc45b793.chunk.js"
+  },
+  {
+    "revision": "7963e25a0a029302be00",
+    "url": "/react-bill-managment/static/js/runtime-main.f566b1e1.js"
+  },
+  {
+    "revision": "1a2efed212fdc6f7982609d9d018e71e",
+    "url": "/react-bill-managment/static/media/search.1a2efed2.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.752a11b6fdeaef38e0742f972e626641.js"
+  "/react-bill-managment/precache-manifest.3811abd8ddf88622e7199319b0e0d956.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-bill-managment/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
